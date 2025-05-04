2433d8fc4ca216e0608bf12fa04eef514787606a
Bechiri Elbatoul <bechirielbatoul@gmail.com>
2025-05-04 23:05:22 +0100

    Add project files.

---
+++ APPWs/Model/ModeleTexte.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APPWs.Model
+{
+    public class ModeleTexte
+    {
+        private string texte;
+
+        public void SetTexte(string input)
+        {
+            texte = input;
+        }
+
+        public string ConvertirEnMajuscules()
+        {
+            char[] resultat = new char[texte.Length];
+            for (int i = 0; i < texte.Length; i++)
+            {
+                char c = texte[i];
+                resultat[i] = char.IsLower(c) ? char.ToUpper(c) : c;
+            }
+            return new string(resultat);
+        }
+    }
+}
---
+++ APPWs/ViewModel/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using APPWs.Model;
+
+namespace APPWs.ViewModel
+{
+    public class ViewModel
+    {
+        private readonly ModeleTexte modele;
+
+        public ViewModel()
+        {
+            modele = new ModeleTexte();
+        }
+
+        public string Convertir(string texte)
+        {
+            modele.SetTexte(texte);
+            return modele.ConvertirEnMajuscules();
+        }
+    }
+}
